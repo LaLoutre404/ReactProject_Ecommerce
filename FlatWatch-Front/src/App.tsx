218f3dedd58c403972d81be57656d84a62baa498
JulienHanon <hanonjulien@outlook.fr>
2023-02-28 15:19:40 +0100

    Définition des routes

--- FlatWatch-Front/src/App.tsx
+++ FlatWatch-Front/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
+import { Route, Routes } from "react-router-dom"
 import Header from './component/Header';
 import Home from './component/Home';
+import Shop from './component/Shop'
 
 
 function App() {
@@ -11,7 +13,10 @@ function App() {
       <Header />
 
       <div>
-        <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
       </div>
     </div>
 
