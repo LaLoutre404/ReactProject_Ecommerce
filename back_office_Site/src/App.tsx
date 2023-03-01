3ae1a6c2573e0eb575dcfdcaba34e2d172fc6223
adrien <a.dubost@safetyn.com>
2023-03-01 14:32:44 +0100

    [FEAT] authentificationProvider and .env file

--- back_office_Site/src/App.tsx
+++ back_office_Site/src/App.tsx
@@ -4,7 +4,8 @@ import ProductList from './components/ProductList'
 import StatsPage from './components/StatistiqueDashboard'
 import { Route, Routes } from 'react-router-dom'
 import { HyperLinkProps } from './components/Header'
-import { MyHeader } from './components/Header'
+import MyHeader  from './components/Header'
+import { AuthProvider } from './components/AuthentificationProvider'
 function App() {
     const linkprops = [{ href: "/", linkname: "Dashboard" },
     { href: "/statistique", linkname: "Statistique" },
@@ -12,12 +13,14 @@ function App() {
 
     return (
         <>
+        <AuthProvider>
             <MyHeader links={linkprops} />
             <Routes>
                 <Route path='/' element={<CommandList />} />
                 <Route path='/statistique' element={<StatsPage />} />
                 <Route path='/liste-produit' element={<ProductList />}></Route>
             </Routes>
+        </AuthProvider>
         </>
     )
 }
