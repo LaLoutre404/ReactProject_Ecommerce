41924d2060553852147f7cceaeed7e7c16f61791
adrien <a.dubost@safetyn.com>
2023-03-01 10:02:37 +0100

    [FEAT] set up firebaseconfig [FIX] props for Header

--- back_office_Site/src/App.tsx
+++ back_office_Site/src/App.tsx
@@ -3,15 +3,22 @@ import CommandList from './components/CommandDashboard'
 import ProductList from './components/ProductList'
 import StatsPage from './components/StatistiqueDashboard'
 import { Route, Routes } from 'react-router-dom'
-
+import { HyperLinkProps } from './components/Header'
+import { MyHeader } from './components/Header'
 function App() {
+    const linkprops = [{ href: "/", linkname: "Dashboard" },
+    { href: "/statistique", linkname: "Statistique" },
+    { href: "/liste-produit", linkname: "Liste Produits" }];
 
     return (
-        <Routes>
-            <Route path='/' element={<CommandList/>}/>
-            <Route path='/statistique' element={<ProductList/>}/>
-            <Route path='/liste-produit' element={<StatsPage/>}></Route>
-        </Routes>
+        <>
+            <MyHeader links={linkprops} />
+            <Routes>
+                <Route path='/' element={<CommandList />} />
+                <Route path='/statistique' element={<ProductList />} />
+                <Route path='/liste-produit' element={<StatsPage />}></Route>
+            </Routes>
+        </>
     )
 }
 
